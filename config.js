b9b5fc8b584a3cd81e7b0a461249109658c591a3
Jvlima22_ <josulima90@gmail.com>
2025-09-04 00:40:24 -0300

    feat: New Website

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js - Arquivo de configuração
+// ATENÇÃO: Este arquivo ainda será público no frontend!
+// Use apenas para chaves que podem ser expostas (como SUPABASE_ANON_KEY)
+
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://cnyowhlgntkgkplwgwkd.supabase.co",
+  SUPABASE_ANON_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNueW93aGxnbnRrZ2twbHdnd2tkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3ODQzODIsImV4cCI6MjA2ODM2MDM4Mn0.1YPIt8ebrOOiuICFaf3PfW7bTz2tG7zJk05U9FBl0Ac",
+  WHATSAPP_NUMBER: "5511993561444",
+};
